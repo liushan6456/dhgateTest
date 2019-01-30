ba56bbf8822e6ecbffcdf42a112032e6cc946faf
Liu Shan <liushan@dhgate.com>
2019-01-30 17:33:33 +0800

    去掉pytest

--- dhgate_test/test_add.py
+++ dhgate_test/test_add.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*
 import unittest
-import pytest
+#import pytest
 # from Cython.Tests import xmlrunner
 # from Cython.Tests.xmlrunner import XMLTestRunner
 
@@ -33,11 +33,11 @@ class TestAdd(unittest.TestCase):
     assert add((1,), (2,)) == (1,2)
 
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
 #   test_suit = unittest.TestSuite()  # 创建一个测试集合
 #   # test_suit.addTest(TestAdd("test_list"))  #测试套件中添加测试用例
 #   test_suit.addTest(unittest.makeSuite(TestAdd))  # 使用makeSuite方法添加所有的测试方法
 #   runner = xmlrunner.XMLTestRunner(output="report")
 #   runner.run(test_suit)
-    pytest.main("-s test_add.py")
+#    pytest.main("test_add.py")
 #   # TestAdd().test_tuple()
