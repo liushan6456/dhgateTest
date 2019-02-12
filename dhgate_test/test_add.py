a2c16b1f03591e1e2073b84e664edc91199d9b9e
Liu Shan <liushan@dhgate.com>
2019-02-12 18:53:43 +0800

    bug

--- dhgate_test/test_add.py
+++ dhgate_test/test_add.py
@@ -4,21 +4,22 @@ import unittest
 # from Cython.Tests import xmlrunner
 # from Cython.Tests.xmlrunner import XMLTestRunner
 class TestAdd(unittest.TestCase):
-  def add(self, a, b):
-    return a + b
+  def test_add(self,a, b):
+      return a + b
   def test_list(self):
-    a = add([1], [2])
+    a = self.test_add([1], [2])
     self.assertEqual(a,[1,2])
-    print(add([1], [2]))
+    print(a)
 
   def test_tuple(self):
-    print(add((1,), (2,)))
     #assert add((1,), (2,)) == (1,2)
-    b = add((1,), (2,))
+    b = self.test_add((1,), (2,))
+    print(b)
     self.assertEqual(b,(1,2))
 
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
+  unittest.main()
    #test_suit = unittest.TestSuite()  # 创建一个测试集合
    #test_suit.addTest(TestAdd("test_list"))  #测试套件中添加测试用例
    #test_suit.addTest(unittest.makeSuite(TestAdd))  # 使用makeSuite方法添加所有的测试方法
